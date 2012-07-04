4bd383c9b416dd60f68b0dd8e739ab3a99a4121c
C J Silverio <ceejceej@gmail.com>
2012-07-04 11:31:50 -0700

    A looping example in the job emitter.
    More of a "how to node" thing than a fivebeans thing, but somebody
    asked.

--- examples/emitjobs.js
+++ examples/emitjobs.js
@@ -4,12 +4,12 @@ var host = '10.0.0.14';
 var port = 11300;
 var tube = 'testtube';
 
-var job1 = { 
+var job1 = {
 	type: 'reverse',
 	payload: 'a man a plan a canal panama'
 };
 
-var job2 = { 
+var job2 = {
 	type: 'emitkeys',
 	payload: {
 		one: 'bloop',
@@ -19,19 +19,43 @@ var job2 = {
 	}
 };
 
+var joblist = [
+	{ type: 'reverse', payload: 'madam, I\'m Adam' },
+	{ type: 'reverse', payload: 'satan oscillate my metallic sonatas' },
+	{ type: 'reverse', payload: 'able was I ere I saw Elba' }
+];
+
+var doneEmittingJobs = function()
+{
+	console.log('We reached our completion callback. Now closing down.');
+	emitter.end();
+	process.exit(0);
+};
+
+var continuer = function(err, jobid)
+{
+	console.log('emitted job id: ' + jobid);
+	if (joblist.length === 0)
+		return doneEmittingJobs();
+
+	emitter.put(0, 0, 60, JSON.stringify(['testtube', joblist.shift()]), continuer);
+};
+
 var emitter = new fivebeans.client(host, port);
 emitter.connect(function(err)
 {
 	emitter.use('testtube', function(err, tname)
 	{
-		console.log("using "+tname);
+		console.log("using " + tname);
 		emitter.put(0, 0, 60, JSON.stringify(['testtube', job1]), function(err, jobid)
 		{
-			console.log('queued a string reverse job in testtube: '+jobid);
+			console.log('queued a string reverse job in testtube: ' + jobid);
 			emitter.put(0, 0, 60, JSON.stringify(['testtube', job2]), function(err, jobid)
 			{
-				console.log('queued a key emitter job in testtube: '+jobid);
-				process.exit(0);
+				console.log('queued a key emitter job in testtube: ' + jobid);
+
+				// And an example of submitting jobs in a loop.
+				emitter.put(0, 0, 60, JSON.stringify(['testtube', joblist.shift()]), continuer);
 			});
 		});
 	});
